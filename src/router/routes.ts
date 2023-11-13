eccebaad1cf4a6d5094cf0fcb21c5acaa5354b9f
SabinaValeriia <valeriia.sabina@onix-systems.com>
2023-11-13 12:33:30 +0200

    Add routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,23 +3,34 @@ import { RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Landing",
-    component: () =>
-      import(/* webpackChunkName: "landing" */ "../views/LandingView.vue"),
+    name: "Auth",
+    component: () => import("../layouts/AuthLayout.vue"),
+    redirect: "/login",
+    children: [
+      {
+        path: "login",
+        name: "login",
+        component: () => import("../views/LoginView.vue"),
+      },
+      {
+        path: "registration",
+        name: "Registration",
+        component: () => import("../views/RegistrationView.vue"),
+      },
+    ],
   },
   {
-    path: "/login",
-    name: "Login",
-    component: () =>
-      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
-  },
-  {
-    path: "/registration",
-    name: "Registration",
-    component: () =>
-      import(
-        /* webpackChunkName: "registration" */ "../views/RegistrationView.vue"
-      ),
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../layouts/MainLayout.vue"),
+    redirect: "/dashboard/project",
+    children: [
+      {
+        path: "projects",
+        name: "projects",
+        component: () => import("../views/ProjectsView.vue"),
+      },
+    ],
   },
 ];
 
