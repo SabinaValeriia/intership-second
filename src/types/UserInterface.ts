62f1590ec1f822d2ace2f63a62d4e752f5a6d6b3
SabinaValeriia <valeriia.sabina@onix-systems.com>
2023-11-15 12:52:46 +0200

    Add pinia

---
+++ src/types/UserInterface.ts
@@ -0,0 +1,10 @@
+export interface UserInterface {
+  id: number;
+  username: string;
+  email: string;
+  provider?: string;
+  confirmed?: boolean;
+  blocked?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
