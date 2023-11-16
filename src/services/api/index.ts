f67e0fc78c85cbd6d698783c6bff722bc1c8c2ac
SabinaValeriia <valeriia.sabina@onix-systems.com>
2023-11-16 11:08:29 +0200

    Add login api fix

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -12,33 +12,4 @@ const axiosInstance = axios.create({
   timeout: 1000,
 });
 
-export const get = <T>(
-  resource: string,
-  config?: AxiosRequestConfig
-): Promise<AxiosResponse<T>> => {
-  return axiosInstance.get<T>(resource, config);
-};
-export const post = <T>(
-  resource: string,
-  data?: T,
-  config?: AxiosRequestConfig
-): Promise<AxiosResponse<T>> => {
-  return axiosInstance.post<T>(resource, data, config);
-};
-
-export const put = <T>(
-  resource: string,
-  data: T,
-  config?: AxiosRequestConfig
-): Promise<AxiosResponse<T>> => {
-  return axiosInstance.put<T>(resource, data, config);
-};
-
-export const deleted = <T>(
-  resource: string,
-  config?: AxiosRequestConfig
-): Promise<AxiosResponse<T>> => {
-  return axiosInstance.delete<T>(resource, config);
-};
-
 export default axiosInstance;
